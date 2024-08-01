63a67742970164b51dd3cef6643795a56106eee2
nurlan1221 <nbayramov592@gmail.com>
2024-08-01 07:45:15 -0700

    first changes

---
+++ src/main/java/az/orient/gittutorial/GittutorialApplication.java
@@ -0,0 +1,13 @@
+package az.orient.gittutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GittutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GittutorialApplication.class, args);
+	}
+
+}
