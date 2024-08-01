f96e0c0873f2723ac27aa3d46e561557406ae215
nurlan1221 <nbayramov592@gmail.com>
2024-08-01 11:50:57 -0700

    write unknown

--- src/main/java/az/orient/gittutorial/GittutorialApplication.java
+++ src/main/java/az/orient/gittutorial/GittutorialApplication.java
@@ -9,7 +9,7 @@ public class GittutorialApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GittutorialApplication.class, args);
 		System.out.println("TEST branch");
-		System.out.println("Nurlan");
+		System.out.println("Unknown");
 	}
 
 }
