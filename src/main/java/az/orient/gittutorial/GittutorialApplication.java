ca3ddfa24c628c193d04ccb4a824652bdd686d83
nurlan1221 <nbayramov592@gmail.com>
2024-08-01 07:57:01 -0700

    remove Hello world

--- src/main/java/az/orient/gittutorial/GittutorialApplication.java
+++ src/main/java/az/orient/gittutorial/GittutorialApplication.java
@@ -9,7 +9,6 @@ public class GittutorialApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(GittutorialApplication.class, args);
-		System.out.println("Hello World!");
 		System.out.println("Nurlan");
 	}
 
