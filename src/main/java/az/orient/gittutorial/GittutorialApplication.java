f222501f14b06da1018bf7dc08b075a70db74a86
nurlan1221 <nbayramov592@gmail.com>
2024-08-03 09:44:49 -0700

    remove

--- src/main/java/az/orient/gittutorial/GittutorialApplication.java
+++ src/main/java/az/orient/gittutorial/GittutorialApplication.java
@@ -8,9 +8,7 @@ public class GittutorialApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GittutorialApplication.class, args);
-		System.out.println("Ali branch");
-		System.out.println("Nurlan branch");
-		System.out.println("X branch");
+		System.out.println("Nurlan");
 
 	}
 
