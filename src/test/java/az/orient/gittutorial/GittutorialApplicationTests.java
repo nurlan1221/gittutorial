8864efeb8edd033852448417c568dedb0c48d82b
nurlan1221 <nbayramov592@gmail.com>
2024-08-03 08:07:48 -0700

    all save

---
+++ src/test/java/az/orient/gittutorial/GittutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package az.orient.gittutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GittutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
